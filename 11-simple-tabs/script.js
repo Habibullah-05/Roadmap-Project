a9d763699539280ceab9fce6a3ecf633e9d3f46f
Habibullah-05 <habibullah.mail05@gmail.com>
2025-10-06 15:23:18 +0700

    11 - Major Update

--- 11-simple-tabs/script.js
+++ 11-simple-tabs/script.js
@@ -1,3 +1,13 @@
 function openTab(evt, tabName) {
-    
+    var i, tabPanel, tablinks;
+    tabPanel = document.getElementsByClassName("tab-panel");
+    for (i = 0; i < tabPanel.length; i++) {
+        tabPanel[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tab-link");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
